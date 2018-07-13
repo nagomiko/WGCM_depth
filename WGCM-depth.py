a29f5ddaffc09b691161ce003a5918523db3f60e
nagomiko <nagomiko@m.local>
2018-07-13 21:20:57 +0900

    first commit

---
+++ WGCM-depth.py
@@ -0,0 +1,31 @@
+MAN = 0
+WOLF = 1
+GOAT = 2
+CABBAGE = 3
+SEARCH_MAX = 20
+left_side = [[]]
+right_side = [[]]
+
+
+def print_state():
+    pass
+
+
+def print_ans():
+    pass
+
+
+def check_state():
+    pass
+
+
+def search():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
